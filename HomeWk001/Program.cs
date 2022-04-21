df9a0e93ff79d05b7f696edaf66359010dec0002
VladimirK <glck666@gmail.com>
2022-04-21 17:10:24 +0500

    home work 1

---
+++ HomeWk001/Program.cs
@@ -0,0 +1,23 @@
+﻿int a = -9 ;
+int b = -3 ;
+int max = 0 ;
+int min = 0 ;
+
+if ( a > b )
+{
+    max = a ; min = b;
+    Console.Write(" Max = ");
+    Console.WriteLine( max );
+    Console.Write(" Min = ");
+    Console.WriteLine( min );
+}
+
+else 
+{ 
+    max = b; min = a;
+    Console.Write(" Max = ");
+    Console.WriteLine( max );
+    Console.Write(" Min = ");
+    Console.WriteLine( min );
+}
+
