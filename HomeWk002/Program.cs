63ed6faf34355e40af04b965cdb00f021c143571
VladimirK <glck666@gmail.com>
2022-04-21 17:14:53 +0500

    home work 2

---
+++ HomeWk002/Program.cs
@@ -0,0 +1,15 @@
+﻿// Это похоже укороченная задача на ту что, мы решали с Ильнаром с гирями)
+// 
+int a = 22;
+int b = 3;
+int c = 9;
+int max = a;
+
+if ( b > max ) max = b;
+if ( c > max ) max = c;
+
+
+Console.Write (" max = " );
+Console.WriteLine ( max );
+
+
