3e83594110edfb7de616bbcc7bf6dcf9b565b52d
VladimirK <glck666@gmail.com>
2022-04-25 18:51:22 +0500

    1 home wk

---
+++ HomeWk2_001/Program.cs
@@ -0,0 +1,16 @@
+﻿int GetNumber()
+{
+    int value = new Random().Next(10, 100);
+    int a = value / 10; // Показывает первое число
+    int b = value % 10; // Показывает второе число
+    //System.Console.WriteLine($"value: {value} a: {a} b: {b}");
+    //проверяем как работает функция
+    int max = a;
+    if (b > max) max = b;
+    return max;
+}
+System.Console.WriteLine(GetNumber());
+
+
+
+
