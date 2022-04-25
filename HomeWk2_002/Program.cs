1b2300937759c00a8526cd05a084082cb3622a99
VladimirK <glck666@gmail.com>
2022-04-25 18:53:01 +0500

    2 home wk

---
+++ HomeWk2_002/Program.cs
@@ -0,0 +1,15 @@
+﻿int num()
+{   
+    int value = 3395;
+    int a = (value / 10) % 10;
+    //System.Console.WriteLine($"value: {value} a: = {a}");
+    //Сначала проверил как работает код, потом из него создал функцию num
+    int num2 =a;
+    return num2;
+
+}    
+System.Console.WriteLine(num());
+    
+
+
+
