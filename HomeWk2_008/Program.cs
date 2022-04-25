3b488ba46d92b5d6d6851e7bb050c7449aef8410
VladimirK <glck666@gmail.com>
2022-04-25 23:13:00 +0500

    8 home wk

---
+++ HomeWk2_008/Program.cs
@@ -0,0 +1,40 @@
+﻿ //честно говоря это все что я смог сделать((        
+int N = 8;
+while (N > 0 && N < 8)
+{
+    if (N < 6)
+        {
+            Console.WriteLine("Не Выходной");
+            break;
+        }
+    else 
+        {
+            Console.WriteLine("Да Выходной");
+            break;
+        }
+}
+//else Console.WriteLine("Такого дня не существует");
+        
+
+
+
+
+
+
+//{
+//       Console.WriteLine("Да Выходной");
+//    }
+//if (N < 6 && N >0)
+  //  {
+    //    Console.WriteLine("Не Выходной");
+  //  }
+     //   else 
+       //     {
+          //      Console.WriteLine("Такого дня не существует");
+        //    }
+    
+
+
+
+
+
