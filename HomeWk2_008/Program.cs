fb25afaf459de2eb5b67ac58a8424d59bee7bc00
VladimirK <glck666@gmail.com>
2022-05-09 15:43:08 +0500

    home mork 8

--- HomeWk2_008/Program.cs
+++ HomeWk2_008/Program.cs
@@ -1,19 +1,26 @@
-﻿ //честно говоря это все что я смог сделать((        
-int N = 8;
-while (N > 0 && N < 8)
-{
-    if (N < 6)
-        {
-            Console.WriteLine("Не Выходной");
-            break;
-        }
-    else 
-        {
-            Console.WriteLine("Да Выходной");
-            break;
-        }
-}
-//else Console.WriteLine("Такого дня не существует");
+﻿int num = 3;
+
+ 
+if (num ==7 || num == 6)
+  {
+    Console.WriteLine("Да Выходной");      
+  } 
+if (num < 6 || num > 0) 
+  { 
+    Console.WriteLine("Рабочий день");
+  } 
+  
+
+ 
+
+
+
+
+
+
+
+
+
         
 
 
@@ -21,18 +28,7 @@ while (N > 0 && N < 8)
 
 
 
-//{
-//       Console.WriteLine("Да Выходной");
-//    }
-//if (N < 6 && N >0)
-  //  {
-    //    Console.WriteLine("Не Выходной");
-  //  }
-     //   else 
-       //     {
-          //      Console.WriteLine("Такого дня не существует");
-        //    }
-    
+
 
 
 
