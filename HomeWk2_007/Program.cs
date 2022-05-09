09ceaf67e4d999da9e9a18f29cf91e85f88fea01
VladimirK <glck666@gmail.com>
2022-05-09 15:42:04 +0500

    home mork 7

---
+++ HomeWk2_007/Program.cs
@@ -0,0 +1,8 @@
+﻿int num (int N)
+{
+    if (N > 99 && N < 100000) return N % 10;
+    return -1;
+}
+int n = 62679;
+if (num(n)==-1) Console.WriteLine("Третьей цифры нет");
+else System.Console.WriteLine(num(n));
