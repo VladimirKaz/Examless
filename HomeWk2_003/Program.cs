5cc4aa22527b0abb4c61acb2b20b430e6e0a339f
VladimirK <glck666@gmail.com>
2022-05-09 15:35:24 +0500

    add 3home mork

---
+++ HomeWk2_003/Program.cs
@@ -0,0 +1,13 @@
+﻿int i = 16;
+int j = 4;
+
+if ( i % j == 0)
+    {
+        Console.WriteLine("Кратно");
+    }
+
+else 
+    {
+        Console.WriteLine("Не кратно, остаток:" );
+        Console.Write(  i % j );
+    }
